493fad17464efbd45ab7efe422a632e18585626f
Jakob Heubl <jakob.heubl@sahnee.dev>
2023-12-13 18:03:12 +0100

    Fixed: Multiple server addresses if active and passive is set

--- agent_scripts/apt_upgrade_agent_2_script.sh
+++ agent_scripts/apt_upgrade_agent_2_script.sh
@@ -12,7 +12,7 @@ TEMP_ZBX_FILE=/tmp/zabbix_apt_check_output.tmp
 echo -n "" > $TEMP_ZBX_FILE
 
 # Check if Server IP/name is set in configuration file
-ZBX_SERVER=$(egrep ^Server= $AGENTD_CONF_FILE | cut -d = -f 2)
+ZBX_SERVER=$(egrep ^Server= $AGENTD_CONF_FILE | cut -d = -f 2 | head -n 1)
 if [ -z "$ZBX_SERVER" ]; then
    echo "Server is not set in zabbix_agent2.conf file"
    exit -1
